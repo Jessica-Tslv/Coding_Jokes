c05fe749d01171aaaea9e04e63e340807b953f37
jessica.tsolova <jessica.tsolova@moonpig.com>
2026-03-31 13:08:04 +0100

    Project initialization - fetches jokes and shows punchline when button clicked

---
+++ index.js
@@ -0,0 +1,33 @@
+const jokeContainer = document.querySelector("div");
+const punchlineButton = document.querySelector("#show-punchline");
+const newJokeButton = document.querySelector("#new-joke");
+
+let currentPunchline = null;
+
+async function fetchJokes() {
+    const response = await fetch("https://official-joke-api.appspot.com/jokes/programming/random");
+    const data = await response.json();
+    const joke = data[0];
+    console.log(joke);
+
+    // To clean old joke
+    jokeContainer.innerHTML = "";
+
+    const setupElement = document.createElement("p");
+    setupElement.textContent = joke.setup;
+
+    const punchlineElement = document.createElement("p")
+    punchlineElement.textContent = joke.punchline;
+
+    currentPunchline = punchlineElement;
+
+    jokeContainer.appendChild(setupElement);
+} 
+punchlineButton.addEventListener('click', () => {
+    if (!jokeContainer.contains(currentPunchline)) {
+        jokeContainer.appendChild(currentPunchline);
+    }
+});
+ 
+
+fetchJokes();
